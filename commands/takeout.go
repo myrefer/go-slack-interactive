b9988b64ca3adc9566e48c88fb63cae57421ad60
eccyan <daisuke.sato@myrefer.co.jp>
2018-11-16 19:59:03 +0900

    Use service account

--- commands/takeout.go
+++ commands/takeout.go
@@ -1,16 +1,11 @@
 package commands
 
 import (
-	"encoding/json"
-	"fmt"
 	api "github.com/nlopes/slack"
 	"log"
-	"net/http"
-	"os"
 
 	"github.com/kelseyhightower/envconfig"
 
-	"golang.org/x/net/context"
 	"golang.org/x/oauth2"
 	"golang.org/x/oauth2/google"
 	"google.golang.org/api/sheets/v4"
@@ -27,16 +22,14 @@ func Takeout(ev *api.MessageEvent, client *api.Client) {
 		log.Fatalf("[ERROR] Failed to process env var: %s", err)
 	}
 
-	config, err := google.ConfigFromJSON(
+	config, err := google.JWTConfigFromJSON(
 		[]byte(env.Credentials),
 		"https://www.googleapis.com/auth/spreadsheets",
 	)
 	if err != nil {
 		log.Fatalf("Unable to parse client secret file to config: %v", err)
 	}
-	googleClient := getClient(config)
-
-	srv, err := sheets.New(googleClient)
+	srv, err := sheets.New(config.Client(oauth2.NoContext))
 	if err != nil {
 		log.Fatalf("Unable to retrieve Sheets client: %v", err)
 	}
@@ -51,59 +44,4 @@ func Takeout(ev *api.MessageEvent, client *api.Client) {
 	if err != nil {
 		log.Fatalf("Unable to retrieve data from sheet: %v", err)
 	}
-}
-
-// Retrieve a token, saves the token, then returns the generated client.
-func getClient(config *oauth2.Config) *http.Client {
-	// The file token.json stores the user's access and refresh tokens, and is
-	// created automatically when the authorization flow completes for the first
-	// time.
-	tokFile := "token.json"
-	tok, err := tokenFromFile(tokFile)
-	if err != nil {
-		tok = getTokenFromWeb(config)
-		saveToken(tokFile, tok)
-	}
-	return config.Client(context.Background(), tok)
-}
-
-// Request a token from the web, then returns the retrieved token.
-func getTokenFromWeb(config *oauth2.Config) *oauth2.Token {
-	authURL := config.AuthCodeURL("state-token", oauth2.AccessTypeOffline)
-	log.Printf("Go to the following link in your browser then type the "+
-		"authorization code: \n%v\n", authURL)
-
-	var authCode string
-	if _, err := fmt.Scan(&authCode); err != nil {
-		log.Fatalf("Unable to read authorization code: %v", err)
-	}
-
-	tok, err := config.Exchange(context.TODO(), authCode)
-	if err != nil {
-		log.Fatalf("Unable to retrieve token from web: %v", err)
-	}
-	return tok
-}
-
-// Retrieves a token from a local file.
-func tokenFromFile(file string) (*oauth2.Token, error) {
-	f, err := os.Open(file)
-	if err != nil {
-		return nil, err
-	}
-	defer f.Close()
-	tok := &oauth2.Token{}
-	err = json.NewDecoder(f).Decode(tok)
-	return tok, err
-}
-
-// Saves a token to a file path.
-func saveToken(path string, token *oauth2.Token) {
-	fmt.Printf("Saving credential file to: %s\n", path)
-	f, err := os.OpenFile(path, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0600)
-	if err != nil {
-		log.Fatalf("Unable to cache oauth token: %v", err)
-	}
-	defer f.Close()
-	json.NewEncoder(f).Encode(token)
 }

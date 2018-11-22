ef6ac3b8f3a969e9028a7323b2eac3db2e4e11ae
ybalexdp <ybalexdp@gmail.com>
2018-11-22 12:28:04 +0900

    add category to assign subcommand

--- commands/assign.go
+++ commands/assign.go
@@ -2,11 +2,24 @@ package commands
 
 import (
 	"fmt"
-	api "github.com/nlopes/slack"
 	"log"
 	"math/rand"
 	"regexp"
 	"time"
+
+	api "github.com/nlopes/slack"
+)
+
+const (
+	APL   = "apl"
+	FRONT = "front"
+	BACK  = "back"
+	ALL   = "all"
+)
+
+const (
+	OK = 0
+	NG = 1
 )
 
 func Assign(ev *api.MessageEvent, client *api.Client) {
@@ -15,15 +28,34 @@ func Assign(ev *api.MessageEvent, client *api.Client) {
 	params := api.NewPostMessageParameters()
 	params.LinkNames = 1
 	params.EscapeText = false
-	message := fmt.Sprintf("やったね <@%s> ちゃん :tada: %s のレビュアーになったまる", assigner(), extructTarget(ev.Text))
+
+	text, err := parse(ev.Text)
+	if err != OK {
+		message := fmt.Sprintf("< assign [ apl | front | back | all ] レビュー対象 > のフォーマットで話しかけてほしいまる")
+	} else {
+		assigner := text[1]
+		target := text[2]
+		message := fmt.Sprintf("やったね <@%s> ちゃん :tada: %s のレビュアーになったまる", assigner, target)
+	}
 	if _, _, err := client.PostMessage(ev.Channel, message, params); err != nil {
 		log.Printf("failed to post message: %s", err)
 	}
 
 }
 
-func members() []string {
-	return []string{"eccyan", "ai", "yuji.ueda", "ueshima", "yabu", "tkatagiri", "iizuka.daisuke"}
+func members(caltegory string) ([]string, int) {
+	switch category {
+	case APL:
+		return []string{"ai", "yuji.ueda", "tkatagiri", "iizuka.daisuke"}, OK
+	case FRONT:
+		return []string{"ai", "yuji.ueda", "tkatagiri", "iizuka.daisuke", "ueshima", "yabu"}, OK
+	case BACK:
+		return []string{"ai", "yuji.ueda", "tkatagiri", "iizuka.daisuke", "yabu"}, OK
+	case ALL:
+		return []string{"ai", "yuji.ueda", "tkatagiri", "iizuka.daisuke", "yabu", "ueshima"}, OK
+	case DEFAULT:
+		return nil, NG
+	}
 }
 
 func normal() float64 {
@@ -31,17 +63,17 @@ func normal() float64 {
 	return rand.NormFloat64()*2.0 + 4.0
 }
 
-func assigner() string {
-	mem := members()
+func assigner(caltegory string) string {
+	mem := members(caltegory)
 	return mem[int(normal()*1234567890)%len(mem)]
 }
 
-func extructTarget(text string) string {
+func parse(text string) ([]string, int) {
 	re := regexp.MustCompile(`assign\s+(.+)$`)
 	match := re.FindStringSubmatch(text)
-	if len(match) == 0 {
-		return ""
+	if len(match) != 3 {
+		return nil, NG
 	}
 
-	return match[1]
+	return match, OK
 }

29686e2f38adf14a33f277694faf77f801c74643
ybalexdp <ybalexdp@gmail.com>
2018-11-22 15:51:00 +0900

    fixed bugs

--- commands/assign.go
+++ commands/assign.go
@@ -77,7 +77,9 @@ func assigner(category string) (string, int) {
 }
 
 func parseParams(text string) ([]string, int) {
-	match, err := strings.Split(test, " ")
-
+	match := strings.Split(test, " ")
+	if len(match) < 2 {
+		return nil, NG
+	}
 	return match, OK
 }

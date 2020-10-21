823ece367120930033a0560be21a205c50cfd28a
ybalexdp <nao.yabu47@gmail.com>
2020-10-21 20:33:14 +0900

    add

--- commands/release.go
+++ commands/release.go
@@ -13,7 +13,7 @@ func Release(ev *api.MessageEvent, client *api.Client) {
 	params.LinkNames = 1
 	params.EscapeText = false
 
-	perm := []string{SUMIYOSHI, UESHIMA, YABUSHITA, NAKAYAMA, TOKUNAGA}
+	perm := []string{SUMIYOSHI, UESHIMA, YABUSHITA, NAKAYAMA, TOKUNAGA, KAMINAGA}
 
 	web, ios, android := assign(perm)
 
